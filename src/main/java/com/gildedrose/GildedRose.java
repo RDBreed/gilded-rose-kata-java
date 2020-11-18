6eca5d9b67f44fcb20611f40ec72fad09ff8b177
rafaela <rafaela.breed@luminis.eu>
2020-11-18 09:44:01 +0100

    Move some local fields to constants

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,14 @@
 package com.gildedrose;
 
 class GildedRose {
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFARAS = "Sulfuras, Hand of Ragnaros";
+    private static final int MAX_DEFAULT_QUALITY = 50;
+    private static final int MINIMAL_QUALITY = 0;
+    private static final int SELL_DATE = 0;
+    private static final int BACKSTAGE_PASSES_SELL_DATE_FIRST_UPGRADE = 11;
+    private static final int BACKSTAGE_PASSES_SELL_DATE_SECOND_UPGRADE = 6;
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -8,52 +16,53 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+        for (int i = MINIMAL_QUALITY; i < items.length; i++) {
+            Item actualItem = items[i];
+            if (!actualItem.name.equals(AGED_BRIE)
+                    && !actualItem.name.equals(BACKSTAGE_PASSES)) {
+                if (actualItem.quality > MINIMAL_QUALITY) {
+                    if (!actualItem.name.equals(SULFARAS)) {
+                        actualItem.quality = actualItem.quality - 1;
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (actualItem.quality < MAX_DEFAULT_QUALITY) {
+                    actualItem.quality = actualItem.quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                    if (actualItem.name.equals(BACKSTAGE_PASSES)) {
+                        if (actualItem.sellIn < BACKSTAGE_PASSES_SELL_DATE_FIRST_UPGRADE) {
+                            if (actualItem.quality < MAX_DEFAULT_QUALITY) {
+                                actualItem.quality = actualItem.quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                        if (actualItem.sellIn < BACKSTAGE_PASSES_SELL_DATE_SECOND_UPGRADE) {
+                            if (actualItem.quality < MAX_DEFAULT_QUALITY) {
+                                actualItem.quality = actualItem.quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!actualItem.name.equals(SULFARAS)) {
+                actualItem.sellIn = actualItem.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+            if (actualItem.sellIn < SELL_DATE) {
+                if (!actualItem.name.equals(AGED_BRIE)) {
+                    if (!actualItem.name.equals(BACKSTAGE_PASSES)) {
+                        if (actualItem.quality > MINIMAL_QUALITY) {
+                            if (!actualItem.name.equals(SULFARAS)) {
+                                actualItem.quality = actualItem.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        actualItem.quality = MINIMAL_QUALITY;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (actualItem.quality < MAX_DEFAULT_QUALITY) {
+                        actualItem.quality = actualItem.quality + 1;
                     }
                 }
             }
