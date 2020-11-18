051009fef1a02d36d984b1ece4ddc603b10ac28c
rafaela <rafaela.breed@luminis.eu>
2020-11-18 10:31:48 +0100

    Let's move the conditions to create a decorator to a factory

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,14 +1,9 @@
 package com.gildedrose;
 
-import com.gildedrose.itemdecorators.AgedBrieDecorator;
-import com.gildedrose.itemdecorators.BackstagePassesDecorator;
-import com.gildedrose.itemdecorators.DefaultItemDecorator;
-import com.gildedrose.itemdecorators.SulfarasDecorator;
+import com.gildedrose.itemdecorators.DecoratorFactory;
+import com.gildedrose.itemdecorators.ItemDecorator;
 
 class GildedRose {
-    private static final String AGED_BRIE = "Aged Brie";
-    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-    private static final String SULFARAS = "Sulfuras, Hand of Ragnaros";
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -17,31 +12,10 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            updateItem(item);
+            final ItemDecorator decorator = DecoratorFactory.createDecorator(item);
+            decorator.update();
         }
     }
 
-    private void updateItem(Item item) {
-        if (isSulfaras(item)) {
-            new SulfarasDecorator(item).update();
-        } else if (isAgedBrie(item)) {
-            new AgedBrieDecorator(item).update();
-        } else if (isBackStagePasses(item)) {
-            new BackstagePassesDecorator(item).update();
-        } else {
-            new DefaultItemDecorator(item).update();
-        }
-    }
 
-    private boolean isSulfaras(Item actualItem) {
-        return actualItem.name.equals(SULFARAS);
-    }
-
-    private boolean isBackStagePasses(Item actualItem) {
-        return actualItem.name.equals(BACKSTAGE_PASSES);
-    }
-
-    private boolean isAgedBrie(Item actualItem) {
-        return actualItem.name.equals(AGED_BRIE);
-    }
 }
---
+++ src/main/java/com/gildedrose/itemdecorators/DecoratorFactory.java
@@ -0,0 +1,37 @@
+package com.gildedrose.itemdecorators;
+
+import com.gildedrose.Item;
+
+public class DecoratorFactory {
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFARAS = "Sulfuras, Hand of Ragnaros";
+
+    private DecoratorFactory(){
+        //not needed since this is a factory (with static methods)
+    }
+
+    public static ItemDecorator createDecorator(Item item){
+        if (isSulfaras(item)) {
+            return new SulfarasDecorator(item);
+        } else if (isAgedBrie(item)) {
+            return new AgedBrieDecorator(item);
+        } else if (isBackStagePasses(item)) {
+            return new BackstagePassesDecorator(item);
+        } else {
+            return new DefaultItemDecorator(item);
+        }
+    }
+
+    private static boolean isSulfaras(Item actualItem) {
+        return actualItem.name.equals(SULFARAS);
+    }
+
+    private static boolean isBackStagePasses(Item actualItem) {
+        return actualItem.name.equals(BACKSTAGE_PASSES);
+    }
+
+    private static boolean isAgedBrie(Item actualItem) {
+        return actualItem.name.equals(AGED_BRIE);
+    }
+}
