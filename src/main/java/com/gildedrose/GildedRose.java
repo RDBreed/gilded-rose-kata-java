2e2c002914ff57ad9061daf4f651c7e0b57a09bc
rafaela <rafaela.breed@luminis.eu>
2020-11-18 09:47:42 +0100

    Create updateItem method & simplify for-loop

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -16,54 +16,58 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = MINIMAL_QUALITY; i < items.length; i++) {
-            Item actualItem = items[i];
-            if (!isAgedBrie(actualItem)
-                    && !isBackStagePasses(actualItem)) {
-                if (isQualityHigherThanMinimalQuality(actualItem)) {
-                    if (!isSulfaras(actualItem)) {
-                        actualItem.quality = actualItem.quality - 1;
+        for (Item item : items) {
+            updateItem(item);
+        }
+    }
+
+    private void updateItem(Item item) {
+        Item actualItem = item;
+        if (!isAgedBrie(actualItem)
+                && !isBackStagePasses(actualItem)) {
+            if (isQualityHigherThanMinimalQuality(actualItem)) {
+                if (!isSulfaras(actualItem)) {
+                    actualItem.quality = actualItem.quality - 1;
+                }
+            }
+        } else {
+            if (isQualityLowerThanMaximalQuality(actualItem)) {
+                actualItem.quality = actualItem.quality + 1;
+
+                if (isBackStagePasses(actualItem)) {
+                    if (actualItem.sellIn < BACKSTAGE_PASSES_SELL_DATE_FIRST_UPGRADE) {
+                        if (isQualityLowerThanMaximalQuality(actualItem)) {
+                            actualItem.quality = actualItem.quality + 1;
+                        }
                     }
+
+                    if (actualItem.sellIn < BACKSTAGE_PASSES_SELL_DATE_SECOND_UPGRADE) {
+                        if (isQualityLowerThanMaximalQuality(actualItem)) {
+                            actualItem.quality = actualItem.quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!isSulfaras(actualItem)) {
+            actualItem.sellIn = actualItem.sellIn - 1;
+        }
+
+        if (isSellInLowerThanSellDate(actualItem)) {
+            if (!isAgedBrie(actualItem)) {
+                if (!isBackStagePasses(actualItem)) {
+                    if (isQualityHigherThanMinimalQuality(actualItem)) {
+                        if (!isSulfaras(actualItem)) {
+                            actualItem.quality = actualItem.quality - 1;
+                        }
+                    }
+                } else {
+                    actualItem.quality = MINIMAL_QUALITY;
                 }
             } else {
                 if (isQualityLowerThanMaximalQuality(actualItem)) {
                     actualItem.quality = actualItem.quality + 1;
-
-                    if (isBackStagePasses(actualItem)) {
-                        if (actualItem.sellIn < BACKSTAGE_PASSES_SELL_DATE_FIRST_UPGRADE) {
-                            if (isQualityLowerThanMaximalQuality(actualItem)) {
-                                actualItem.quality = actualItem.quality + 1;
-                            }
-                        }
-
-                        if (actualItem.sellIn < BACKSTAGE_PASSES_SELL_DATE_SECOND_UPGRADE) {
-                            if (isQualityLowerThanMaximalQuality(actualItem)) {
-                                actualItem.quality = actualItem.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!isSulfaras(actualItem)) {
-                actualItem.sellIn = actualItem.sellIn - 1;
-            }
-
-            if (isSellInLowerThanSellDate(actualItem)) {
-                if (!isAgedBrie(actualItem)) {
-                    if (!isBackStagePasses(actualItem)) {
-                        if (isQualityHigherThanMinimalQuality(actualItem)) {
-                            if (!isSulfaras(actualItem)) {
-                                actualItem.quality = actualItem.quality - 1;
-                            }
-                        }
-                    } else {
-                        actualItem.quality = MINIMAL_QUALITY;
-                    }
-                } else {
-                    if (isQualityLowerThanMaximalQuality(actualItem)) {
-                        actualItem.quality = actualItem.quality + 1;
-                    }
                 }
             }
         }
